945dfdc5d3ad8af054e52916171a27d628e99781
Arpit Namdev <arpit8392@gmail.com>
2023-06-13 22:40:52 +0530

    Fix Image Shrinking Issue in case of Private messages

--- components/NotificationItem.tsx
+++ components/NotificationItem.tsx
@@ -26,45 +26,49 @@ const NotificationItem = ({ message, clickHandler }: Props) => {
 		<li
 			onClick={clickHandler}
 			className={classNames(
-				'flex items-center gap-3 p-4 hover:cursor-pointer md:gap-5 md:p-5',
+				'flex justify-between p-4 hover:cursor-pointer md:p-5',
 				!isRead ? 'rounded-lg bg-veryLightGrayishBlue' : 'bg-inherit'
 			)}>
-			<div className='relative h-10 w-10 self-start rounded-full md:h-12 md:w-12'>
-				<Image
-					src={`/images/${activityByImage}`}
-					alt={activityBy}
-					fill
-					className='object-contain'
-				/>
-			</div>
-			<div className='flex flex-col gap-[3px]'>
-				<p className='max-w-xs text-sm text-darkGrayishBlue md:max-w-none md:text-base'>
-					<span className='font-extrabold text-veryDarkBlue hover:cursor-pointer hover:text-blue'>
-						{activityBy}
-					</span>{' '}
-					{activity}{' '}
-					{activityAdditionalDetails && (
-						<span className='font-bold hover:cursor-pointer hover:text-blue'>
-							{activityAdditionalDetails}
-						</span>
-					)}
-					{!isRead && (
-						<span className='mx-[6px] inline-block h-2 w-2 rounded-full bg-red'></span>
-					)}
-				</p>
-
-				<p className='text-sm text-grayishBlue md:text-base'>{elapsedTime}</p>
-				{hasPrivateMessage && (
-					<p className='my-3 rounded-md border border-lightGrayishBlue2 p-4 text-sm text-darkGrayishBlue hover:bg-lightGrayishBlue1 md:p-5 md:text-base'>
-						{privateMessage}
+			<div className='flex gap-3 md:gap-5 '>
+				<div className='relative h-10 w-10 self-start rounded-full md:h-12 md:w-12 shrink-0'>
+					<Image
+						src={`/images/${activityByImage}`}
+						alt={activityBy}
+						fill
+						className='object-contain'
+					/>
+				</div>
+				<div className='flex flex-col gap-[3px]'>
+					<p className='max-w-xs text-sm text-darkGrayishBlue md:max-w-none md:text-base'>
+						<span className='font-extrabold text-veryDarkBlue hover:cursor-pointer hover:text-blue'>
+							{activityBy}
+						</span>{' '}
+						{activity}{' '}
+						{activityAdditionalDetails && (
+							<span className='font-bold hover:cursor-pointer hover:text-blue'>
+								{activityAdditionalDetails}
+							</span>
+						)}
+						{!isRead && (
+							<span className='mx-[6px] inline-block h-2 w-2 rounded-full bg-red'></span>
+						)}
 					</p>
-				)}
+
+					<p className='text-sm text-grayishBlue md:text-base'>{elapsedTime}</p>
+
+					{hasPrivateMessage && (
+						<blockquote className='my-3 rounded-md border border-lightGrayishBlue2 p-4 text-sm text-darkGrayishBlue hover:bg-lightGrayishBlue1 md:p-5 md:text-base '>
+							{privateMessage}
+						</blockquote>
+					)}
+				</div>
 			</div>
+
 			{activityImage && (
-				<div className='relative h-10 w-10 translate-x-16 rounded-full md:h-12 md:w-12 md:translate-x-44'>
+				<div className='relative h-10 w-10 rounded-full md:h-12 md:w-12'>
 					<Image
 						src={`/images/${activityImage}`}
-						alt=''
+						alt={activityImage}
 						fill
 						className='object-contain'
 					/>
